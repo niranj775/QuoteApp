280bedd9a8b32e85c94fc3b579ac48d28a1009f4
niranj775 <niranj775@gmail.com>
2025-02-14 20:17:51 +0530

    Final App

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("1");
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const apiUrl ='https://api.gameofthronesquotes.xyz/v1/random';
+
+async function getQuote(url) {
+    const response = await fetch(url);
+    var data = await response.json();
+    quote.innerHTML = data.sentence;
+    author.innerHTML = data.character.name;
+}
+
+getQuote(apiUrl);
+
+function tweet(){
+    let xUrl =`https://twitter.com/intent/tweet?text=${quote.innerHTML}`+ ' --- by ' + author.innerHTML+'.';
+    window.open(xUrl, 'Tweet Window', 'width=600, height=300');
+}
